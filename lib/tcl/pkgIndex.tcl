a93f861174391e5cdb517a963bdcad51ebd28acd
Feng Ding <opsteev@gmail.com>
2015-06-10 16:04:44 +0800

    Arsenic

---
+++ lib/tcl/pkgIndex.tcl
@@ -0,0 +1,3 @@
+package ifneeded LOG 1.1 [list source [file join $dir log.lib]]
+package ifneeded CLI 1.1 [list source [file join $dir cli.lib]]
+package ifneeded DEV 1.1 [list source [file join $dir dev.lib]]
